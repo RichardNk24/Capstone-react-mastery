959d6e629d78b088f4cd47879ba3a327282fa478
RichardNk24 <richardsobelnkulu@gmail.com>
2023-03-18 21:58:27 +0200

    Rendering Home Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Outlet } from "react-router-dom"
 import Home from "./components/routes/home/Home.component"
 const App = () => {
+  
+  const Navigation = () => {
+      return (
+        <div>
+          <div>
+            <h1>I am the navigation bar</h1>
+          </div>
+          <Outlet />
+        </div>
+      )
+    
+  }
+
+  const Shop = () => {
+    return <h1>Shop Page</h1> 
+  }
 
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+      
     </Routes>
   )
 }
--- src/components/routes/home/Home.component.jsx
+++ src/components/routes/home/Home.component.jsx
@@ -1,7 +1,13 @@
+import { Outlet } from 'react-router-dom'
 import Directory from '../../../components/categories/Categories.component'
 
 const Home = () => {
-    return <Directory />
+    return (
+        <div>
+            <Directory />
+            <Outlet />
+        </div>
+    )
     
 }
 
